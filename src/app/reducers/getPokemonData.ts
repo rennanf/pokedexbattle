02fb570688afa125bfd3546d2f99fcb0bd176fbb
Rennan Fabricio <rennan.fabricio@simplesmeiosdepagamento.com.br>
2024-03-26 16:58:56 -0300

    segunda att pokedex_battle2.0

---
+++ src/app/reducers/getPokemonData.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { generatedPokemonType, genericPokemonType  } from "../../utils/Types";
+import axios from "axios";
+import { defaultImages, images } from "../../utils/getPokemonimages";
+import { pokemonTypes } from "../../utils/getPokemonTypes";
+
+export const getPokemonData = createAsyncThunk("pokemon/randomPokemon",async(pokemons:genericPokemonType[])=> {
+    try {
+        // console.log({pokemons}, "from reducer");
+        const pokemonsData:generatedPokemonType[] = [];
+        for await(const pokemon of pokemons){
+            const {data,
+            }: {
+                data: {
+                    id: number;
+                types:{type:generatedPokemonType}[];
+                }
+            } = await axios.get(pokemon.url);
+            const types = data.types.map(
+                ({ type: { name } }: { type: { name: string } }) => ({
+                    // @ts-expect-error
+                  [name]: pokemonTypes[name],
+                })
+              );
+            // @ts-expect-error
+            // console.log({defaultImages, images});
+            let image: string = images[data.id];
+            if(!image){
+                // @ts-expect-error
+                image = defaultImages[data.id];
+            }
+
+            if (image) {
+                pokemonsData.push({
+                    name: pokemon.name,
+                    id: data.id,
+                    image,
+                    types,
+                });
+            }
+
+            console.log({pokemonsData});
+        }
+    }catch(err) {
+
+    }
+})
--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -9,3 +9,21 @@ export interface genericPokemonType {
     name: string;
     url: string;
 }
+
+export interface generatedPokemonType {
+    name:string;
+    id:number;
+    image:string;
+        types:pokemonTypeInterface[];
+}
+
+export interface pokemonTypeInterface{
+    [key:string] : {
+        image:string;
+        resistance: string[];
+        strength: string[];
+        weakness: string[];
+        vulnerable: string[];
+    };
+}
+
