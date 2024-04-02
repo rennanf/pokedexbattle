ce1e57fb5d877bff3c80a72891708cb780afd20d
Rennan <rennanfabricio22@gmail.com>
2024-04-01 21:43:30 -0300

    atualização pequena/casa

--- src/components/CompareContainer.tsx
+++ src/components/CompareContainer.tsx
@@ -11,27 +11,28 @@ function CompareContainer({
 }) {
     const getStats = () => {};
     return (
-        <div className="compare-container">
-            {isEmpty && (
-                <div className="empty">
-                    <button>
-                        <FaPlus />
-                    </button>
-                    <h3>Adicione o pokemon para comparar</h3>
-                </div>
-            )}
-            {pokemon && (
-                <div className="compare-element">
-                    <div className="compare-info">
-                        <div className="compare-details">
-                            <h3>{pokemon?.name}</h3>
-                            <img src={pokemon?.image} alt="pokemon" className="compare-image" />
-                        </div>
-                        <div className="pokemon-types-container">
+      <div className="compare-container">
+      {isEmpty && (
+        <div className="empty">
+          <button>
+            <FaPlus />
+          </button>
+          <h3>Adicione o pokemon para comparar</h3>
+        </div>
+      )}
+      {pokemon && (
+        <div className="compare-element">
+          <div className="compare-info">
+            <div className="compare-details">
+              <h3>{pokemon?.name}</h3>
+              <img src={pokemon?.image} alt="pokemon" className="compare-image" />
+            </div>
+            <div className="pokemon-types-container">
               <div className="pokemon-types">
                 <h4 className="pokemon-type-title">Type</h4>
                 <div className="pokemon-type-icons">
-                  {pokemon?.types.map((type: pokemonTypeInterface) => {
+                  {
+                  pokemon?.types.map((type: pokemonTypeInterface) => {
                     const keys = Object.keys(type);
                     return (
                       <div className="pokemon-type">
@@ -50,14 +51,15 @@ function CompareContainer({
             </div>
           </div>
           <div className="compare-action-buttons">
-                            <button className="compare-btn">Add</button>
-                            <button className="compare-btn">View</button>
-                            <button className="compare-btn">Remove</button>
-                        </div>
-                </div>
-            )}
-            
+            <button className="compare-btn">Adicionar</button>
+            <button className="compare-btn">Detalhes</button>
+            <button className="compare-btn">Remover</button>
+          </div>
         </div>
+
+      )}
+    </div>
+    
         
     );
     
